ae24941f1eea853078290be695018b8bef5a009d
bagong vit <bagongvit@gmail.com>
2024-06-28 11:12:13 +0700

    main

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    @include('home.css')
+</head>
+            
+            <body>
+              <div class="hero_area">
+                <!-- header section strats -->
+                @include('home.header')
+                <!-- end header section -->
+                <!-- slider section -->
+            
+                @include('home.slider')
+            
+                <!-- end slider section -->
+              </div>
+              <!-- end hero area -->
+            
+              <!-- shop section -->
+            
+              @include('home.product')
+            
+              <!-- end shop section -->
+            
+            
+            
+            
+            
+            
+            
+              <!-- contact section -->
+            
+              
+            
+              <!-- end contact section -->
+            
+               @include('home.contact')
+            
+              <!-- info section -->
+            
+              @include('home.footer')
+            
+            </body>
+            
+            </html></h1>
+
+</html>
